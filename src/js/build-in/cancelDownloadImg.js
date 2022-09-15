6ae9ee05cda57dbc8ee87d005881ac80efb6d053
Albert <kamalov00albert@gmail.com>
2022-09-15 11:49:03 +0300

    Правки

--- src/js/build-in/cancelDownloadImg.js
+++ src/js/build-in/cancelDownloadImg.js
@@ -2,3 +2,5 @@ export default function cancelDownloadImg() {
     const images = document.querySelectorAll('img');
     images.forEach(img => img.addEventListener('contextmenu', e => e.preventDefault()))
 }
+
+window.initCancelDownloadImg = cancelDownloadImg;
--- src/js/build-in/fancybox.js
+++ src/js/build-in/fancybox.js
@@ -2,6 +2,13 @@ import { Fancybox } from '@fancyapps/ui';
 
 export default function fancybox() {
     Fancybox.bind('[data-fancybox]', {
-
+        Image: {
+            zoom: false
+        },
+        on: {
+            load: (fancybox, slide) => {
+                window.initCancelDownloadImg();
+            },
+        },
     })
 }
