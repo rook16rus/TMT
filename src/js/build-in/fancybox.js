4afffbb8fbdfe44d20c5a4b927f5fd7939886d2f
rook16rus <kamalov00albert@gmail.com>
2022-10-13 19:49:01 +0300

    Правки

--- src/js/build-in/fancybox.js
+++ src/js/build-in/fancybox.js
@@ -2,8 +2,22 @@ import { Fancybox } from '@fancyapps/ui';
 
 export default function fancybox() {
     Fancybox.bind('[data-fancybox]', {
+        helpers     : {
+            overlay : {
+                speedIn  : 0,
+                speedOut : 300,
+                opacity  : 0.8,
+                css      : {
+                    cursor : 'default'
+                },
+                closeClick: false
+            }
+        },
         Image: {
-            zoom: false
+            zoom: false,
+            Panzoom: {
+                ratio: 3
+            },
         },
         on: {
             load: (fancybox, slide) => {
